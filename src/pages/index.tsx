f29ded7fd656475d522b0ff75886529bec900402
Patrick Youssef <p_youss@live.concordia.ca>
2021-04-13 19:05:19 -0400

    remove useless files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@ import Home from './home/Home';
 import Login from './login/Login';
 import AdvancedSearch from './advancedSearch/AdvancedSearch';
 import BookInfo from './home/book-info/BookInfo';
-import AdvancedTable from './home/advanced-table/AdvancedTable'
 
 export {
   Home,
   Login,
   AdvancedSearch,
   BookInfo,
-  AdvancedTable
 };
